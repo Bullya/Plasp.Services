eb400d91a02cda69cfd6895b7f0a262db2759d11
Hejinxiong <hejinxiong@flytexpress.com>
2019-09-02 16:34:34 +0800

    create oa project

---
+++ Plasp.Express/Plasp.Express.OA/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Plasp.Express.OA.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
